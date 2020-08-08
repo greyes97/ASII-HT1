6a5be974b5a71757e9f6652c8da6b17bdf000745
edfern <ed_fern@hotmail.com>
2020-08-08 17:17:30 -0600

    Inicio de proyecto

---
+++ src/main/java/gt/com/antiguaburger/antiguaburgerApp/AntiguaburgerAppApplication.java
@@ -0,0 +1,13 @@
+package gt.com.antiguaburger.antiguaburgerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiguaburgerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntiguaburgerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gt/com/antiguaburger/antiguaburgerApp/AntiguaburgerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.com.antiguaburger.antiguaburgerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiguaburgerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
